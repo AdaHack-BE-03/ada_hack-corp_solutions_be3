b6440c9248766fdaa299732c8fea02f76b480dda
Heloiza Mendes <helocursos@gmail.com>
2024-04-17 02:25:55 -0300

    Correção de indicadores

--- src/app/controllers/indicator.controller.js
+++ src/app/controllers/indicator.controller.js
@@ -1,86 +1,54 @@
 
+
  import { User } from "../../infra/db/sequelize/models/index.js";
-
-
 
 class IndicatorController {
   static async countByGroup(req, res) {
     try {
       const counts = {};
 
-      if (req.body.nationality) {
-        counts.nationality = await User.count({
-          group: ['nationality'],
-          attributes: ['nationality']
-        });
-      } else {
-        counts.nationality = 0;
-      }
+   
+      console.log("Consulting and counting over_60...");
+      counts.over_60 = await User.count({
+        where: { over_60: true }
+      });
 
-      if (req.body.over_60) {
-        counts.over_60 = await User.count({
-          group: ['over_60'],
-          attributes: ['over_60']
-        });
-      } else {
-        counts.over_60 = 0;
-      }
+     
+      console.log("Consulting and counting gender...");
+      counts.gender = await User.count({
+        where: { gender: true }
+      });
 
-      if (req.body.education_level) {
-        counts.education_level = await User.count({
-          group: ['education_level'],
-          attributes: ['education_level']
-        });
-      } else {
-        counts.education_level = 0;
-      }
+      console.log("Consulting and counting ethnicity...");
+      counts.ethnicity = await User.count({
+        where: { ethnicity: true }
+      });
 
-      if (req.body.gender) {
-        counts.gender = await User.count({
-          group: ['gender'],
-          attributes: ['gender']
-        });
-      } else {
-        counts.gender = 0;
-      }
+      console.log("Consulting and counting sexual_orientation...");
+      counts.sexual_orientation = await User.count({
+        where: { sexual_orientation: true }
+      });
 
-      if (req.body.ethnicity) {
-        counts.ethnicity = await User.count({
-          group: ['ethnicity'],
-          attributes: ['ethnicity']
-        });
-      } else {
-        counts.ethnicity = 0;
-      }
+      console.log("Consulting and counting pcd...");
+      counts.pcd = await User.count({
+        where: { pcd: true }
+      });
 
-      if (req.body.sexual_orientation) {
-        counts.sexual_orientation = await User.count({
-          group: ['sexual_orientation'],
-          attributes: ['sexual_orientation']
-        });
-      } else {
-        counts.sexual_orientation = 0;
-      }
-
-      if (req.body.pcd) {
-        counts.pcd = await User.count({
-          group: ['pcd'],
-          attributes: ['pcd']
-        });
-      } else {
-        counts.pcd = 0;
-      }
-
+      console.log("Counts:", counts);
       res.json(counts);
     } catch (error) {
+      console.error("Error:", error);
       res.status(500).json({ error: 'Internal server error' });
     }
   }
 }
 
-
-
 export default IndicatorController;
+
+
+
+
+
 
 
 
